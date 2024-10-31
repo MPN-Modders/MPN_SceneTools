154198733910615ceec8b3d772221c65b5771873
XDefault <75306339+ExDefault@users.noreply.github.com>
2024-10-31 23:06:28 +0100

    another type
    fixed typo

--- Assets/Code/SceneObjs/ProxyObj_Playground.cs
+++ Assets/Code/SceneObjs/ProxyObj_Playground.cs
@@ -2,7 +2,7 @@
 
 public class ProxyObj_Playground : ProxyObj
 {
-    [Tooltip("The Playground control room will mask away a wall its the window isn't obstructed. \n\nWARNING: This will also mask out a ton of visual effects, like blood and selection outlines. We recommend cutting a hole n your geometry for the window to line up with.")]
+    [Tooltip("The Playground control room will mask away a wall its the window isn't obstructed. \n\nWARNING: This will also mask out a ton of visual effects, like blood and selection outlines. We recommend cutting a hole in your geometry for the window to line up with.")]
     public bool ApplyWindowMask = false;
     [Tooltip("Enable scaffolding visuals.")]
     public bool ScaffoldLegs = false;
